cfaab30b4a865b154e7b2785f04c1a7201afb535
Nolan Whitecotton <Z1912101@students.niu.edu>
2021-01-19 21:41:08 -0600

    Linked the UnitTest1 dependencies

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,21 +1,39 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+#include "../MagicSquare/src/MagicSquareBruteForcer.h"
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace UnitTest1
-{
-	TEST_CLASS(UnitTest1)
-	{
+namespace UnitTest1{
+	TEST_CLASS(UnitTest1){
 	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-			Assert::AreEqual(1, 2);
+
+		TEST_METHOD(TestGenericTemplateConstructor) {
+			//construct
+			SquareTemplate *st = new SquareTemplate(true, 3, 1, 9, false); //-m 9 -c -s 3
+
+			//asserts
+			Assert::AreEqual(st->getIsCompact(), true);
+			Assert::AreEqual(st->getSquareSize(), 3);
+			Assert::AreEqual(st->getRecurMax(), 9);
+			Assert::AreEqual(st->getRecurOffset(), 1);
+			Assert::AreEqual(st->getShowIdentical(), false);
 		}
 
-		TEST_METHOD(customtest) 
-		{
+		TEST_METHOD(TestSquareIntConstructor) {
+			//prep
+			SquareTemplate *st = new SquareTemplate(true, 3, 1, 9, false); //-m 9 -c -s 3
+
+			//construct
+			Square* sq = new Square(3, st);
+
+			//asserts
+			Assert::AreEqual(sq->getSize(),3);
+			
+		}
+
+		TEST_METHOD(test) {
 			Assert::AreEqual(1, 1);
 		}
 	};
